6eb0040b461242d7b70026a624c065ff141f7817
shrey-Odinproject <sonishreyf1@gmail.com>
2021-10-29 18:18:51 +0530

    created a proper 16 by 16 grid and styled it a bit , added the hover effect on div elm of grid

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const mainDiv=document.querySelector('.mainDiv')
-for(let i =0;i<36;i++){
+for(let i =0;i<256;i++){
     const gridDiv=document.createElement('div')
     gridDiv.classList.add('gridDiv')
     mainDiv.appendChild(gridDiv)
 }
+const gridDivNodeList=document.querySelectorAll('.gridDiv')
+
+gridDivNodeList.forEach(divElm=>{divElm.addEventListener('mouseover',(e)=>e.target.classList.add('hovering'))
+divElm.addEventListener('mouseout',(e)=>e.target.classList.remove('hovering'))})
