18c2e4c52ac653cd84f6bce95a9424bd1c42c077
shrey-Odinproject <sonishreyf1@gmail.com>
2021-10-29 18:51:10 +0530

    added a reset button that clears the grid and styled a bit

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const mainDiv=document.querySelector('.mainDiv')
-for(let i =0;i<256;i++){
-    const gridDiv=document.createElement('div')
+const body=document.querySelector('body')
+const mainDiv = document.querySelector('.mainDiv')
+for (let i = 0; i < 256; i++) {
+    const gridDiv = document.createElement('div')
     gridDiv.classList.add('gridDiv')
     mainDiv.appendChild(gridDiv)
-}
-const gridDivNodeList=document.querySelectorAll('.gridDiv')
+}// created divs using for loop
+const gridDivNodeList = document.querySelectorAll('.gridDiv')
 
-gridDivNodeList.forEach(divElm=>{divElm.addEventListener('mouseover',(e)=>e.target.classList.add('hovering'))
-divElm.addEventListener('mouseout',(e)=>e.target.classList.remove('hovering'))})
+// adding hover feature for all divs
+gridDivNodeList.forEach(divElm => {
+    divElm.addEventListener('mouseover', (e) => e.target.classList.add('hovering'))
+    //divElm.addEventListener('mouseout', (e) => e.target.classList.remove('hovering'))
+
+})
+
+const reset = document.createElement('button')
+reset.textContent='RESET'
+body.appendChild(reset)
+
+reset.addEventListener('click', (e) => gridDivNodeList.forEach(divElm =>divElm.classList.remove('hovering')))// resets all divs
