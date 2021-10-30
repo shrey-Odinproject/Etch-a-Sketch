4b2e41cfa8c91f19ecf984070abcb8a721233c04
shrey-Odinproject <sonishreyf1@gmail.com>
2021-10-31 00:02:54 +0530

    added hovering to reset button , created a buttonDiv div which will house all the buttons and shifted reset inside it , also created another button

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let oneDivSize=parseInt(prompt('entr dimension')) // asks the dimension of grid to user
+let oneDivSize // globally declaring 
+do {
+    oneDivSize = parseInt(prompt('entr dimension'));
+} // asks the dimension of grid to user}
+while (oneDivSize > 100); // only accepts value less than 50 else it asks again
 
 const body = document.querySelector('body')
 const mainDiv = document.querySelector('.mainDiv')
-mainDiv.setAttribute('style', `height :${500}px;width:${500}px`)
-for (let i = 0; i < oneDivSize*oneDivSize; i++) {
+
+mainDiv.setAttribute('style', `height :${584.6}px;width:${584.6}px`) // main div is 500x500
+
+for (let i = 0; i < oneDivSize * oneDivSize; i++) {
     const gridDiv = document.createElement('div')
     gridDiv.classList.add('gridDiv')
     mainDiv.appendChild(gridDiv)
@@ -15,11 +21,25 @@ const gridDivNodeList = document.querySelectorAll('.gridDiv')
 gridDivNodeList.forEach(divElm => {
     divElm.addEventListener('mouseover', (e) => e.target.classList.add('hovering'))
     //divElm.addEventListener('mouseout', (e) => e.target.classList.remove('hovering'))
-    divElm.setAttribute('style', `height :${500 / oneDivSize}px;width:${500 / oneDivSize}px`) // here i setted width and height of a single grid div
-})
+    divElm.setAttribute('style', `height :${584.6 / oneDivSize}px;width:${584.6 / oneDivSize}px`) // here i setted width and height of a single grid div
+})// above line causes bug for strange values like 7,13,17 i.e grid is filled strangly
+
+const buttonDiv=document.createElement('div')
+buttonDiv.classList.add('buttondiv')
+buttonDiv.setAttribute('style','width:500px')
+body.appendChild(buttonDiv)
 
 const reset = document.createElement('button')
 reset.textContent = 'RESET'
-body.appendChild(reset)
+buttonDiv.appendChild(reset)
 
 reset.addEventListener('click', (e) => gridDivNodeList.forEach(divElm => divElm.classList.remove('hovering')))// resets all divs
+reset.addEventListener('mouseover', (e) => e.target.classList.add('hoveringbtn'))
+reset.addEventListener('mouseout', (e) => e.target.classList.remove('hoveringbtn'))
+
+/*const RGB = document.createElement('button')
+RGB.textContent = 'RGB'
+buttonDiv.appendChild(RGB)
+
+RGB.addEventListener('mouseover', (e) => e.target.classList.add('hoveringbtn'))
+RGB.addEventListener('mouseout', (e) => e.target.classList.remove('hoveringbtn'))*/
