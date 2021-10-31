d4d5627260b23b89aa70c6a767443c72acb8887c
shrey-Odinproject <sonishreyf1@gmail.com>
2021-10-31 12:43:37 +0530

    added the rgb feature but it has to be toggled on manually , created an rgb button that toggles value of rgb variable , further implementation is pending

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const body = document.querySelector('body')
 const mainDiv = document.querySelector('.mainDiv')
 
+let rgb=true
+
+
 const mainDivDimensions = '500px'
 let gridDimension; // stores the dimension of grid eg 10 ,4 (translates to 10x10 4x4)
 do {
@@ -24,13 +27,10 @@ function makeMainDivChilds(dimension) { //function to create grid divs eg if 10 
 }
 
 makeMainDivChilds(gridDimension) // creates divs by calling function
+const gridDivNodeList = document.querySelectorAll('.gridDiv') // nodelist of all griddivs created
 
-const gridDivNodeList = document.querySelectorAll('.gridDiv') // nodelist of all griddivs
-gridDivNodeList.forEach(divElm => {
-    divElm.addEventListener('mouseover', (e) => e.target.classList.add('hovering'))
-})
 
-const buttonDiv = document.createElement('div') // this will store all divs
+const buttonDiv = document.createElement('div') // this will store all buttons
 buttonDiv.classList.add('buttondiv')
 buttonDiv.setAttribute('style', `width:${mainDivDimensions}`)
 body.appendChild(buttonDiv)
@@ -39,6 +39,39 @@ const reset = document.createElement('button')
 reset.textContent = 'RESET'
 buttonDiv.appendChild(reset)
 
-reset.addEventListener('click', (e) => gridDivNodeList.forEach(divElm => divElm.classList.remove('hovering')))// resets all divs
-reset.addEventListener('mouseover', (e) => e.target.classList.add('hoveringbtn'))
-reset.addEventListener('mouseout', (e) => e.target.classList.remove('hoveringbtn'))
+const RGB=document.createElement('button')
+RGB.textContent='RGB'
+buttonDiv.appendChild(RGB)
+
+const allButtons = document.querySelectorAll('.buttondiv button') //all buttons in buttondiv
+allButtons.forEach(button => {// common action for all buttons
+    
+    button.addEventListener('mouseover', (e) => e.target.classList.add('hoveringbtn'))
+    button.addEventListener('mouseout', (e) => e.target.classList.remove('hoveringbtn'))
+    
+})
+// below are button specific action
+reset.addEventListener('click', () => gridDivNodeList.forEach(divElm => divElm.classList.remove('hovering')))// resets all divs
+reset.addEventListener('click', () => gridDivNodeList.forEach(divElm => divElm.setAttribute('style',`background:aquamarine`)))// this is for when rgb is true and u wanna reset
+RGB.addEventListener('click',()=>console.log(rgb=!rgb)) // on click RGB button we toggle value of rgb variable)
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }// RETURNS A RANDOM COLOR
+
+gridDivNodeList.forEach(divElm => { // what will happen to each div element
+    if (rgb===false){ // value of rgb variable determines what will happen on hover event 
+    divElm.addEventListener('mouseover', (e) => e.target.classList.add('hovering'))
+    }
+    else{divElm.addEventListener('mouseover', (e) => e.target.setAttribute('style',`background:${getRandomColor()}`))}
+    
+})
+
+
+// IMPLEMENTING CHANGING rgb VALUE VIA BUTTON CLICK AND UPDATING THE GRID ON BUTTON CLICK IS LEFT 
+// PROGRAMMS WORKS IF WE MANUALLLY CHANGE rgb I NOW HAVE TO INCORPORATE BUTTON AND DYNAMICALLY EDITING GRID
